59d247f64df324b6239ccaa30481e58162febbaf
kyepesl <kyepesl16@gmail.com>
2021-12-11 21:36:31 -0500

    Se realizan cambios en visualizacion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  URL: "https://app-citas-veterinarias.herokuapp.com/api"
+  URL: "https://app-citas-veterinarias.herokuapp.com/api",
+  AuthUrl: "https://app-citas-veterinarias.herokuapp.com",
+  AppUser: "citasvetapp",
+  AppPassword: "1234567"
 };
